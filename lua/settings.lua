0580a70901943ea11581cd0bd7b93439b8491a95
GuilhermeMelow <guilherme.felipe65@outlook.com>
2024-11-26 21:36:03 -0300

    Corrige executável.

--- lua/settings.lua
+++ lua/settings.lua
@@ -33,4 +33,4 @@ o.inccommand = "split" -- When nonempty, shows the effects of :substitute, :smag
 vim.cmd[[colorscheme dracula]]
 
 -- Only works on windows :)
-vim.opt.shell = "pwsh"
+vim.opt.shell = "powershell.exe"
